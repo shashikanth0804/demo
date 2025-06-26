251f90e2b985f91f25e4143a25a8871a4ccbe7a3
P Shashi Kanth Reddy <shashikanth8938@gmail.com>
2025-06-26 23:23:50 +0530

    Initial commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
+import tailwindcss from '@tailwindcss/postcss';
+
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    tailwindcss: tailwindcss(),
+  },
 };
 
 export default config;
