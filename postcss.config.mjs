3d01dae9c25eeab263a5c642ff5c52aa8aac9881
shashikanth0804 <pshashikanthr@gmail.com>
2025-06-28 20:55:59 +0530

    Updated postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/postcss';
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: tailwindcss(),
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
